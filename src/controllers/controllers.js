027faebffdc3683381024683a94d17db1f7c55b7
jhor pama <jkpama@misena.edu.co>
2019-08-06 10:26:30 -0500

    Se crea la ruta y el controlador

---
+++ src/controllers/controllers.js
@@ -0,0 +1,9 @@
+const Articulo = require('../models/articulo');
+const Bodega = require('../models/bodega');
+const Log = require('../models/logs');
+
+const bodega = {};
+
+bodega.getArticulos = async (req, res) => {
+  
+}
